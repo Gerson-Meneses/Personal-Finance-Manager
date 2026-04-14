b857c1897cf8f236443fe84748c6d3a26d000795
Gerson-Meneses <gersonmneneses1612@gmail.com>
2026-04-14 09:13:13 -0500

    Finance_Manager_1.04

--- backend/src/config/nodemailer.ts
+++ backend/src/config/nodemailer.ts
@@ -1,12 +1,24 @@
 import nodemailer, { Transporter } from 'nodemailer';
 import 'dotenv/config'
 
-const transporter: Transporter = nodemailer.createTransport({
+/* const transporter: Transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
         user: process.env.EMAIL_USER, 
         pass: process.env.EMAIL_PASSWORD,
     },
+}); */
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        type: 'OAuth2',
+        user: process.env.EMAIL_USER,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        refreshToken: process.env.REFRESH_TOKEN,
+    },
 });
+
 
 export default transporter;
